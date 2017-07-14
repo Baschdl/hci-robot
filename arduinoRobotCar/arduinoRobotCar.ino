03f77b34e86ac841234e9eabe379e37e3ef44069
Juliane <julianekleinknecht@gmail.com>
2017-07-14 12:07:12 +0200

    added led lights

--- arduinoRobotCar/arduinoRobotCar.ino
+++ arduinoRobotCar/arduinoRobotCar.ino
@@ -60,6 +60,7 @@ int currentColor;
 boolean lastGreyWhite = false;
 boolean forward = true;
 int correctDirectionCount = 0;
+boolean won = false;
 //-------------------------------------------------------------------------------------------------
 
 //------------------------------------------initialization-----------------------------------------
@@ -72,6 +73,10 @@ void setup() {
   }
 
   pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
   digitalWrite(led1, HIGH);
   delay(1000);
   digitalWrite(led1, LOW);
@@ -128,12 +133,61 @@ void loop() {
     delay(100);
     Stop();
   }
+
+  if(won){
+    digitalWrite(led1, HIGH);
+    delay(50);
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, HIGH);
+    delay(50);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, HIGH);
+    delay(50);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, HIGH);
+    delay(50);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, HIGH);
+    delay(50);
+    digitalWrite(led5, LOW);
+  }
 }
 
-void stateUpdate(int colorOfCurrentStrip){
-  lastColor = currentColor;
-  currentColor = colorOfCurrentStrip;
+
+//----------------------------------drive to aim-----------------------------------------------
+void driveToBrightness()
+{
+   if(lastColor == currentColor){
+    //color is the same we are in the same strip we drive straight
+    driveForward();
+   }
+   else{
+    if((lastColor == black && currentColor == white) || (lastColor == grey && currentColor == black) || (lastColor == white && currentColor == grey)){
+      if(lastColor == white && currentColor == grey && lastGreyWhite){
+        Serial.print("whuuup whuup we won");
+        won = true;
+        digitalWrite(led2, HIGH);
+        digitalWrite(led3, HIGH);
+        driveBackward();
+        delay(20);
+        Stop();
+      }else{
+        changeDirection();
+        driveForward(); 
+      }
+      lastGreyWhite = false;
+    }
+    else{
+      if(lastColor == grey && currentColor == white){
+        lastGreyWhite = true;
+      }else{
+        lastGreyWhite = false;
+      }
+      driveForward(); 
+    }
+   }
 }
+
 
 //gives back the color to the given sensorvalue
 int colorOfStrip(int sensorvalue){
@@ -197,6 +251,8 @@ boolean checkDirection(){
   }
 }
 
+//TODO
+
 void possibleErrorPrevention(){
   Serial.println("Error, now driving around like crazy");
   int leftColor = colorOfStrip(leftPinValue);
@@ -208,40 +264,49 @@ void possibleErrorPrevention(){
 }
 
 void correctDirection(int leftColor, int rightColor){
+  int rotationTime = 20;
+  int forwardTime = 10;
   if(currentColor != leftColor){
+    digitalWrite(led4, HIGH);
     if(forward){
       RotateRight();
-      delay(20);
+      delay(rotationTime);
       Forward();
-      delay(10);
+      delay(forwardTime);
       Stop();
     }
     else{
       RotateLeft();
-      delay(20);
+      delay(rotationTime);
       Backward();
-      delay(10);
+      delay(forwardTime);
       Stop();
     }
-      
+    digitalWrite(led4, LOW);
   }
   else{
+    digitalWrite(led5, HIGH);
     if(forward){
       RotateLeft();
-      delay(20);
+      delay(rotationTime);
       Backward();
-      delay(10);
+      delay(forwardTime);
       Stop();
     }
     else{
       RotateRight();
-      delay(20);
+      delay(rotationTime);
       Forward();
-      delay(10);
+      delay(forwardTime);
       Stop();
     }
-      
+    digitalWrite(led5, LOW);  
   }
+}
+
+void stateUpdate(int colorOfCurrentStrip){
+  lastColor = currentColor;
+  currentColor = colorOfCurrentStrip;
 }
 
 //----------------------------------readSensors-------------------------------------------------------
@@ -254,33 +319,6 @@ void readSensors()
   Serial.print("read sensors");
 }
 
-//
-//----------------------------------determine direction-----------------------------------------------
-void driveToBrightness()
-{
-	 if(lastColor == currentColor){
-    //color is the same we are in the same strip we drive straight
-    driveForward();
-   }
-   else{
-    if((lastColor == black && currentColor == white) || (lastColor == grey && currentColor == black) || (lastColor == white && currentColor == grey)){
-      if(lastColor == white && currentColor == grey && lastGreyWhite){
-        Serial.print("whuuup whuup we won");
-        Stop();
-      }else{
-        changeDirection();
-        driveForward(); 
-      }
-    }
-    else{
-      if(lastColor == grey && currentColor == white){
-        lastGreyWhite = true;
-      }
-      driveForward(); 
-    }
-   }
-}
-
 void changeDirection(){
   forward = !forward;
 }
@@ -290,6 +328,15 @@ void driveForward(){
   }
   else{
     Backward();
+  }
+}
+
+void driveBackward(){
+  if(forward){
+    Backward();
+  }
+  else{
+    Forward();
   }
 }
 //
@@ -312,37 +359,37 @@ void printLightValues(int forward, int backward, int left, int right)
 void Forward()
 {
   Serial.println("move forward");
-  /*digitalWrite(motor1PinA, HIGH);
+  digitalWrite(motor1PinA, HIGH);
   digitalWrite(motor1PinB, LOW);
   digitalWrite(motor2PinA, HIGH);
-  digitalWrite(motor2PinB, LOW);*/
+  digitalWrite(motor2PinB, LOW);
 }
 
 void Backward() // inverse voltage of both motors
 {
   Serial.println("move backward");
-  /*digitalWrite(motor1PinA, LOW);
+  digitalWrite(motor1PinA, LOW);
   digitalWrite(motor1PinB, HIGH);
   digitalWrite(motor2PinA, LOW);
-  digitalWrite(motor2PinB, HIGH);*/
+  digitalWrite(motor2PinB, HIGH);
 }
 
 void RotateLeft() // inverse voltage of only one motor
 {
   Serial.println("rotate left");
-  /*digitalWrite(motor1PinA, LOW);
+  digitalWrite(motor1PinA, LOW);
   digitalWrite(motor1PinB, HIGH);
   digitalWrite(motor2PinA, HIGH);
-  digitalWrite(motor2PinB, LOW);*/
+  digitalWrite(motor2PinB, LOW);
 }
 
 void RotateRight() // inverse voltage of only one motor
 {
   Serial.println("rotate right");
-  /*digitalWrite(motor1PinA, HIGH);
+  digitalWrite(motor1PinA, HIGH);
   digitalWrite(motor1PinB, LOW);
   digitalWrite(motor2PinA, LOW);
-  digitalWrite(motor2PinB, HIGH);*/
+  digitalWrite(motor2PinB, HIGH);
 }
 
 void Stop() 
